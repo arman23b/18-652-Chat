38191ffcf5c4a26667ed4d8bee084e0b2df0185f
arman23b <arman23b@gmail.com>
2014-12-29 12:08:13 +0600

    Changed mongolab_uri

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// 'url' : 'mongodb://localhost/test'
-	'url' : 'mongodb://arman23b@gmail.com:Electronika_23@ds043220.mongolab.com:43220/heroku_app32635536'
+	'url' : 'mongodb://heroku_app32635536:vfslrd9lantfvrbm8latuldgh0@ds043220.mongolab.com:43220/heroku_app32635536'
 }
